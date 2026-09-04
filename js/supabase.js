c24b3835a6c11dedef68e2bc9daa8035fafb4c33
ly6572586-create <ly6572586@gmail.com>
2026-09-05 01:26:40 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,15 @@
+/* =========================================
+   Supabase
+   دوري ميدي للمحترفين 2026
+   ========================================= */
 
+const SUPABASE_URL =
+    "https://zvrskcferjktyjkrbtdp.supabase.co";
+
+const SUPABASE_ANON_KEY =
+    "sb_publishable_GzjhjEuoRjoaamIiNUhARA_7qDXYQoJ";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
